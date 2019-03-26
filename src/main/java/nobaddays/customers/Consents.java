5e508451d81a4f92ffef9644e3533bc5611a5bbe
Andre Erasmus <andre.erasmus@entelect.co.za>
2019-03-26 13:47:35 +0200

    Initial commit

---
+++ src/main/java/nobaddays/customers/Consents.java
@@ -0,0 +1,10 @@
+package nobaddays.customers;
+
+class Consents {
+
+    private Consents() {
+    }
+
+    static final String DEFAULT_JSON_TXT_INPUT_URL = "https://s3.amazonaws.com/intercom-take-home-test/customers.txt";
+
+}
---
+++ src/main/java/nobaddays/customers/CustomersApplication.java
@@ -0,0 +1,49 @@
+package nobaddays.customers;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+@SpringBootApplication
+public class CustomersApplication {
+
+	public static void main(String[] args) throws IOException, JSONException {
+		SpringApplication.run(CustomersApplication.class, args);
+		JSONObject json = readJsonFromUrl(Consents.DEFAULT_JSON_TXT_INPUT_URL);
+
+		System.out.println(json);
+		System.out.println(json.get("name"));
+	}
+
+	private static String readAll(Reader rd) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		int cp;
+		while ((cp = rd.read()) != -1) {
+			sb.append((char) cp);
+		}
+		return sb.toString();
+	}
+
+	private static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+		InputStream is = new URL(url).openStream();
+		try {
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+			String jsonText = readAll(rd);
+			JSONObject json = new JSONObject(jsonText);
+			return json;
+		} finally {
+			is.close();
+		}
+	}
+	
+}
