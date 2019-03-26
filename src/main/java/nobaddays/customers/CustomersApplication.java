21e17992066409fb97fec24e942cf29219138d0a
Andre Erasmus <andre.erasmus@entelect.co.za>
2019-03-26 22:17:53 +0200

    Add Basic Error handeling

--- src/main/java/nobaddays/customers/CustomersApplication.java
+++ src/main/java/nobaddays/customers/CustomersApplication.java
@@ -20,26 +20,30 @@ public class CustomersApplication {
 
 	private static final Logger LOGGER = Logger.getLogger(CustomersApplication.class.getName());
 
-	public static void main(String[] args) throws IOException, JSONException {
-		SpringApplication.run(CustomersApplication.class, args);
+	public static void main(String[] args){
+        SpringApplication.run(CustomersApplication.class, args);
 
         LOGGER.log(Level.INFO, () -> "Getting List of customers within " +
                 Constants.RANGE_TO_CHECK_IN_KM + "km Range of (" + Constants.OFFICE_GPS_COORDINATE + ")");
 
-		List<Customer> customers = FileReader.getCustomers(new URL(Constants.JSON_TXT_FILE_INPUT_URL).openStream());
-		List<Customer> customersInRange = CustomerUtils.getCustomersWithinDistance(
-				customers, Constants.OFFICE_GPS_COORDINATE, Constants.RANGE_TO_CHECK_IN_KM);
+	    try {
+            List<Customer> customers = FileReader.getCustomers(Constants.JSON_TXT_FILE_INPUT_URL);
+            List<Customer> customersInRange = CustomerUtils.getCustomersWithinDistance(
+                    customers, Constants.OFFICE_GPS_COORDINATE, Constants.RANGE_TO_CHECK_IN_KM);
 
-        LOGGER.log(Level.INFO, () -> "Total customers read from file " + customers.size() + ", of which " +
-                customersInRange.size() + " are within a " + Constants.RANGE_TO_CHECK_IN_KM + " Range.");
+            LOGGER.log(Level.INFO, () -> "Total customers read from file " + customers.size() + ", of which " +
+                    customersInRange.size() + " are within a " + Constants.RANGE_TO_CHECK_IN_KM + " Range.");
 
-        customersInRange.sort(Comparator.comparingInt(Customer::getUserId));
-        StringBuilder sb = new StringBuilder();
-        for (Customer customer : customersInRange) {
-            sb.append(customer).append(lineSeparator());
+            customersInRange.sort(Comparator.comparingInt(Customer::getUserId));
+            StringBuilder sb = new StringBuilder();
+            for (Customer customer : customersInRange) {
+                sb.append(customer).append(lineSeparator());
+            }
+
+            LOGGER.log(Level.INFO, () -> "List of customers in range: " + lineSeparator() + sb);
+        } catch (Exception e){
+	        LOGGER.log(Level.SEVERE, "An Error has occurred, Exiting Application: " + e.toString());
         }
-
-        LOGGER.log(Level.INFO, () -> "List of customers in range: " + lineSeparator() + sb);
 
 	}
 
