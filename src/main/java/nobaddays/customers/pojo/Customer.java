fec7eb42ee78d848698f172c0b79eb559560f62e
Andre Erasmus <andre.erasmus@entelect.co.za>
2019-03-28 15:11:54 +0200

    Add Lombok, to reduce boilerplate code
    No More Getters, Setters and All args constructors

--- src/main/java/nobaddays/customers/pojo/Customer.java
+++ src/main/java/nobaddays/customers/pojo/Customer.java
@@ -1,40 +1,15 @@
 package nobaddays.customers.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Customer {
 
     private int userId;
     private String name;
     private GPSCoordinate location;
-
-    public Customer(int userId, String name, GPSCoordinate location) {
-        this.userId = userId;
-        this.name = name;
-        this.location = location;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public GPSCoordinate getLocation() {
-        return location;
-    }
-
-    public void setLocation(GPSCoordinate location) {
-        this.location = location;
-    }
 
     @Override
     public String toString(){
--- src/main/java/nobaddays/customers/pojo/GPSCoordinate.java
+++ src/main/java/nobaddays/customers/pojo/GPSCoordinate.java
@@ -1,30 +1,14 @@
 package nobaddays.customers.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class GPSCoordinate {
 
     private double latitude;
     private double longitude;
-
-    public GPSCoordinate(double latitude, double longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
 
     @Override
     public String toString(){
