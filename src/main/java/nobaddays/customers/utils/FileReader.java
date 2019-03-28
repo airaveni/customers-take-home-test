f316c470a4eefba7764e845cde8d490fdfcddf6f
Andre Erasmus <andre.erasmus@entelect.co.za>
2019-03-28 12:54:35 +0200

    Add Unit Test for getCustomers method
    Test if getCustomers reads an InputStream as expected

--- src/main/java/nobaddays/customers/utils/FileReader.java
+++ src/main/java/nobaddays/customers/utils/FileReader.java
@@ -9,7 +9,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,10 +18,9 @@ public class FileReader {
     private FileReader() {
     }
 
-    public static List<Customer> getCustomers(String url) throws IOException {
+    public static List<Customer> getCustomers(InputStream inputStream) throws IOException {
         List<JSONObject> customers = new ArrayList<>();
 
-        InputStream inputStream = new URL(url).openStream();
         try (BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream, Charset.forName(Constants.JSON_TXT_FILE_ENCODING)))) {
             String line;
             while ((line = rd.readLine()) != null) {
--- src/test/java/nobaddays/customers/utils/FileReaderTest.java
+++ src/test/java/nobaddays/customers/utils/FileReaderTest.java
@@ -5,13 +5,33 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileReaderTest {
 
     @Test
-    public void getCustomers() {
+    public void getCustomers() throws IOException {
+
+        String exampleString = "{\"latitude\": \"52.986375\", \"user_id\": 12, \"name\": \"Christina McArdle\", \"longitude\": \"-6.043701\"}\n" +
+                "{\"latitude\": \"51.92893\", \"user_id\": 1, \"name\": \"Alice Cahill\", \"longitude\": \"-10.27699\"}\n" +
+                "{\"latitude\": \"51.8856167\", \"user_id\": 2, \"name\": \"Ian McArdle\", \"longitude\": \"-10.4240951\"}\n" +
+                "{\"latitude\": \"52.3191841\", \"user_id\": 3, \"name\": \"Jack Enright\", \"longitude\": \"-8.5072391\"}";
+
+        InputStream inputStream = new ByteArrayInputStream(exampleString.getBytes(StandardCharsets.UTF_8));
+
+        List<Customer> customers = FileReader.getCustomers(inputStream);
+
+        assert (customers.size() == 4);
+
+    }
+
+    @Test
+    public void getCustomersListFromJsonList() {
 
         List<JSONObject> customerJsonObjects = new ArrayList<>();
 
